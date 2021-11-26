a63ce745fec563bf79c90b36332b002bb83ea137
Alonesa <alenakrasnov@mail.ru>
2021-11-26 15:24:52 +0200

    added greetings

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from 'uuid';
 
 // types
 export type UserType = {
@@ -12,7 +13,8 @@ function HW3() {
     const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
     const addUserCallback = (name: string) => { // need to fix any
-        setUsers([]) // need to fix
+      const user = {_id: v1(), name: name}
+      setUsers([user, ...users]) // need to fix
     }
 
     return (
