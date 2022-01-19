44ebb7f71a85ba1805252fba9ad9da2b829fbda3
Alonesa <alenakrasnov@mail.ru>
2022-01-19 18:55:24 +0200

    files for check routin

---
+++ src/p2-homeworks/h1/Menu.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Menu = () => {
+  return (
+	<div>
+	  <nav>
+		<li><a href="/#/homework1">Homework1</a></li>
+		<li><a href="/#/homework2">Homework2</a></li>
+		<li><a href="/#/homework3">Homework3</a></li>
+		<li><a href="/#/homework4">Homework4</a></li>
+		<li><a href="/#/homework5">Homework5</a></li>
+	  </nav>
+	</div>
+  );
+};
+
