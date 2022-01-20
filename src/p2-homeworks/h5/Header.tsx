cda6e84170b59313abf5a216c51aaf1417975b31
Alonesa <alenakrasnov@mail.ru>
2022-01-20 23:30:52 +0200

    connect homework6 file

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -6,41 +6,43 @@ import {PATH} from './Routs';
 function Header() {
   let [active, setActive] = useState(false);
   const menuButtonHandler = () => {
-    setActive(!active)
+	setActive(!active)
   }
   
   const styleHandler = () => {
-    return active ? s.menu__button__pressed : s.menu__button
+	return active ? s.menu__button__pressed : s.menu__button
   }
   
   
   return (
-    <div>
-      <nav className={s.header}>
-        <div
-          className={styleHandler()}
-          onClick={menuButtonHandler}>Меню
-        </div>
-        <div className={s.header__items}>
-          <NavLink to={PATH.HW1}
-                   className={(navData) => navData.isActive ? s.active : ''}>Homework
-            1</NavLink>
-          <NavLink to={PATH.HW2}
-                   className={(navData) => navData.isActive ? s.active : ''}>Homework
-            2</NavLink>
-          <NavLink to={PATH.HW3}
-                   className={(navData) => navData.isActive ? s.active : ''}>Homework
-            3</NavLink>
-          <NavLink to={PATH.HW4}
-                   className={(navData) => navData.isActive ? s.active : ''}>Homework
-            4</NavLink>
-          <NavLink to={PATH.PRE_JUNIOR}
-                   className={(navData) => navData.isActive ? s.active : ''}>Pre
-            Junior</NavLink>
-            </div>
-          </nav>
-      </div>
-    )
+	<div>
+	  <nav className={s.header}>
+		<div
+		  className={styleHandler()}
+		  onClick={menuButtonHandler}>Меню
+		</div>
+		<div className={s.header__items}>
+		  <NavLink to={PATH.HW1}
+				   className={(navData) => navData.isActive ? s.active : ''}>Homework
+			1</NavLink>
+		  <NavLink to={PATH.HW2}
+				   className={(navData) => navData.isActive ? s.active : ''}>Homework
+			2</NavLink>
+		  <NavLink to={PATH.HW3}
+				   className={(navData) => navData.isActive ? s.active : ''}>Homework
+			3</NavLink>
+		  <NavLink to={PATH.HW4}
+				   className={(navData) => navData.isActive ? s.active : ''}>Homework
+			4</NavLink>
+		  <NavLink to={PATH.PRE_JUNIOR}
+				   className={(navData) => navData.isActive ? s.active : ''}>Pre
+			Junior</NavLink>
+		  <NavLink to={PATH.HW6}
+				   className={(navData) => navData.isActive ? s.active : ''}>Homework 6</NavLink>
+		</div>
+	  </nav>
+	</div>
+  )
 }
 
 export default Header
