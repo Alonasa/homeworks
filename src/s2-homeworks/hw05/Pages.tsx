7f0560505490e4b2e2ba68ba246192b8b9453301
Alonesa <alenakrasnov@mail.ru>
2022-10-27 17:23:50 +0300

    added routes to pages

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,33 +1,40 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+  PRE_JUNIOR: '/pre-junior',
+  JUNIOR: '/junior',
+  JUNIOR_PLUS: '/junior-plus',
+  ERROR: '/404',
 }
 
 function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
-            <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
-
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
-            </Routes>
+          <Routes>
+            
+  
+  
+            {/*роутинг будут писать студенты*/}
+            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+            <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+    
+            {/*роуты для /pre-junior, /junior, /junior-plus*/}
+            <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+            <Route path={PATH.JUNIOR} element={<Junior/>}/>
+            <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+    
+    
+            {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+            <Route path='*' element={<Navigate to="/404" replace/>}/>
+            <Route path='/404' element={<Error404/>}/>
+  
+          </Routes>
         </div>
     )
 }
