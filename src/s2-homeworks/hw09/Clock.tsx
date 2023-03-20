3c991655bb73c54bf13f444c268d6ae9a3303e91
Alonesa <alenakrasnov@mail.ru>
2023-03-20 23:11:08 +0000

    added current month on hover

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -74,15 +74,16 @@ function Clock() {
     return `${date?.getDay().toString().padStart(2,'0')}.${(date?.getMonth()+1).toString().padStart(2,'0')}.${date?.getFullYear()}`
   }
   
+  const currentMonth = () => {
+	return date.toLocaleString('default', { month: 'long' })
+  }
+  
   const stringTime = currentTime() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  
-  
-  
   const stringDate = currentDate() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
   
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
   const stringDay = currentDay() || <br/> // пишут студенты
-  const stringMonth = 'date->month' || <br/> // пишут студенты
+  const stringMonth = currentMonth() || <br/> // пишут студенты
   
   return (
 	<div className={s.clock}>
