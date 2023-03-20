84d3dc9d1f3688894f31bed601882a86ed7cd41c
Alonesa <alenakrasnov@mail.ru>
2023-03-20 23:04:44 +0000

    refactor time format, added formatted date on hover

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -42,7 +42,7 @@ function Clock() {
   
   
   const currentTime = () => {
-	return `${date?.getHours() < 9 ? '0' + date.getHours() : date.getHours()}:${date?.getMinutes() < 9 ? '0' + date.getMinutes() : date.getMinutes()}:${date?.getSeconds() < 9 ? '0' + date.getSeconds() : date.getSeconds()}`
+	return `${date?.getHours().toString().padStart(2,'0')}:${date?.getMinutes().toString().padStart(2,'0')}:${date?.getSeconds().toString().padStart(2,'0')}`
   }
   
   const currentDay = () => {
@@ -71,10 +71,13 @@ function Clock() {
   }
   
   const currentDate = () => {
-    return date.getDate()
+    return `${date?.getDay().toString().padStart(2,'0')}.${(date?.getMonth()+1).toString().padStart(2,'0')}.${date?.getFullYear()}`
   }
   
   const stringTime = currentTime() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+  
+  
+  
   const stringDate = currentDate() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
   
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
