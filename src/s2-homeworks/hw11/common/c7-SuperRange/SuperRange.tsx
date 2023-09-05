df210cb5b62d6e95e202207707ff683bf0f6851d
Alonesa <alenakrasnov@mail.ru>
2023-09-05 17:42:52 +0100

    fix styles for thumb on hover as it's default reset element look

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -10,7 +10,10 @@ export const StyledSlider = styled(Slider)(({ theme }) => ({
     height: 15,
     width: 15,
     boxShadow: '0 0 0 7px white, 0 0 0 8px green, 0 0 0 9px yellow, 0 0 0 10px green',
-  }
+    '&:hover': {
+      boxShadow: '0 0 0 7px white, 0 0 0 8px green, 0 0 0 10px yellowgreen, 0 0 0 12px green',
+    },
+  },
 }))
 
 const SuperRange: React.FC<SliderProps> = (props) => {
